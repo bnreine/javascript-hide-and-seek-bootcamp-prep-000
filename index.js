88c855fbdd52ce287f20a4b2f5237792b2387634
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 19:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ function getFirstSelector(selector) {
 
 /*
 
+
+
+- Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
 */
