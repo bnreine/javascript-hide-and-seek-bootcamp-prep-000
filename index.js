fb9d06a7d91651ddc9324d3372fb9bd3faf5d77e
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 20:02:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lists=document.querySelectorAll('.ranked-list li');
+  for (var i=0;i<lists.length;i++) {
+    lists[i].innerHTML=parseInt(lists[i].innerHTML)+3;
+  }
+}
+
+function deepestChild() {
+  var tempNode = document.querySelector('#grand-node');
+  while (tempNode.querySelector('div')!=null) {
+    tempNode=tempNode.querySelector('div');
+  }
+  return tempNode;
+}
