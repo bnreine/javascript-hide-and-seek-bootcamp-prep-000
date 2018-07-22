9359f9c006f59f732aae2e72d295db38ab150bf9
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 19:00:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 
 
 /*
-
+describe('index', () => {
+  describe('getFirstSelector(selector)', () => {
+    it('returns the first element that matches the selector', () => {
+      expect(getFirstSelector('div').id).to.equal('nested')
+      expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
+    })
+  })
 
 - Define a function `getFirstSelector(selector)`, which accepts a selector and returns the first element that matches.
 */
