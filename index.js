64bb821502086f24f9be5631ff38f7df5ec112df
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 19:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,29 +15,5 @@ function increaseRankBy(n) {
 
 
 /*
-  describe('increaseRankBy(n)', () => {
-    it('increases ranks in .ranked-list by n', () => {
-      increaseRankBy(3)
 
-      const rankedLists = document.querySelectorAll('.ranked-list')
-      const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
-
-      let children = firstList.children
-      let start = 1
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
-      }
-
-      children = secondList.children
-      start = 12
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start - i + 3)
-      }
-    })
-  })
-})
-
-- Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
 */
