1e8a7d2f291913f12c2b787ab527d146c7d4d0c7
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 19:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var lists=document.querySelectorAll('.ranked-list li');
   for (var i=0;i<lists.length;i++) {
-    lists[i].innerHTML=lists[i].innerHTML
+    lists[i].innerHTML=parseInt(lists[i].innerHTML)+3;
   }
 }
 
