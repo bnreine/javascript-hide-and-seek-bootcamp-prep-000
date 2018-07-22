10d746ad4b08a572fc7d74591494a7571f1052d0
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-22 20:02:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,3 @@ function deepestChild() {
   }
   return tempNode;
 }
-
-
-/*
-  describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
-      expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
-    })
-  })
-
-
-- Define a function `deepestChild()` that pulls out the most deeply nested child
-from `div#grand-node`. (Remember, you can iterate over elements and call
-`querySelector()` and `querySelectorAll()` on them. This is challenging to
-implement correctly, but not beyond your ability!)
-*/
